8b45d7af0d6812bff00e71e819a77e2af8c15a96
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-05-16 12:34:45 -0300

    Add banner and messages

--- che.sh
+++ che.sh
@@ -1,8 +1,42 @@
 #!/usr/bin/env bash
 
-npm_bin=$(npm bin)
+end=$(     printf '\033'[00m )
+red=$(     printf '\033'[31m )
+green=$(   printf '\033'[32m )
+yellow=$(  printf '\033'[33m )
 
-echo "This takes a while"
+npm_bin=$(npm bin)
+echo $red'
+          _____                    _____                    _____
+         /\    \                  /\    \                  /\    \
+        /::\    \                /::\____\                /::\    \
+       /::::\    \              /:::/    /               /::::\    \
+      /::::::\    \            /:::/    /               /::::::\    \
+     /:::/\:::\    \          /:::/    /               /:::/\:::\    \
+    /:::/  \:::\    \        /:::/____/               /:::/__\:::\    \
+   /:::/    \:::\    \      /::::\    \              /::::\   \:::\    \
+  /:::/    / \:::\    \    /::::::\    \   _____    /::::::\   \:::\    \
+ /:::/    /   \:::\    \  /:::/\:::\    \ /\    \  /:::/\:::\   \:::\    \
+/:::/____/     \:::\____\/:::/  \:::\    /::\____\/:::/__\:::\   \:::\____\
+\:::\    \      \::/    /\::/    \:::\  /:::/    /\:::\   \:::\   \::/    /
+ \:::\    \      \/____/  \/____/ \:::\/:::/    /  \:::\   \:::\   \/____/
+  \:::\    \                       \::::::/    /    \:::\   \:::\    \
+   \:::\    \                       \::::/    /      \:::\   \:::\____\
+    \:::\    \                      /:::/    /        \:::\   \::/    /
+     \:::\    \                    /:::/    /          \:::\   \/____/
+      \:::\    \                  /:::/    /            \:::\    \
+       \:::\____\                /:::/    /              \:::\____\
+        \::/    /                \::/    /                \::/    /
+         \/____/                  \/____/                  \/____/
+
+'$end
+
+function message
+{
+  echo "$green$1$end"
+}
+
+message 'Calculating the largest prime number divisible by two...'
 
 npm cache clean --silent
 npm install --no-spin --silent
@@ -11,4 +45,9 @@ bower install --silent
 
 export PATH="$npm_bin:$PATH"
 
-ember --version
+ember_cli_version=$(ember --version | grep ^version | awk '{ print $2 }')
+
+message "It seems to be two."
+echo
+message "${yellow}Using ember-cli version ${green}$ember_cli_version"
+message 'Done.'
